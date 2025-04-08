aee67b69baf23579f3383c6a2badeee56050b5f6
Chris0Jeky <jeky.tck@gmail.com>
2025-04-08 03:26:02 +0100

    Update run_statistical_analysis.py

--- run_statistical_analysis.py
+++ run_statistical_analysis.py
@@ -3,3 +3,7 @@ import os
 import json
 import logging
 
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from npdl.analysis.analysis import compare_scenarios_stats
+from npdl.core.logging_utils import setup_logging # For logging configuration

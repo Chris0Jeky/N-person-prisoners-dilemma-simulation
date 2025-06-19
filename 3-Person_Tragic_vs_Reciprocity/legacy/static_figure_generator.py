bccb71c73b82a2a3e38d3789dded3dbb6eab01d1
Chris0Jeky <jeky.tck@gmail.com>
2025-06-19 13:37:54 +0100

    Update static_figure_generator.py

--- 3-Person_Tragic_vs_Reciprocity/legacy/static_figure_generator.py
+++ 3-Person_Tragic_vs_Reciprocity/legacy/static_figure_generator.py
@@ -247,7 +247,8 @@ def run_multiple_simulations_extended(simulation_func, agents, num_rounds, num_r
             fresh_agents.append(StaticAgent(
                 agent_id=agent.agent_id,
                 strategy_name=agent.strategy_name,
-                exploration_rate=agent.exploration_rate
+                exploration_rate=agent.exploration_rate,
+                exploration_decay=agent.exploration_decay
             ))
         
         tft_history, coop_history, score_history = simulation_func(fresh_agents, num_rounds)
